2fe584129fb5aba9ee4b9ac88e4344ece8815a37
KaterynaKalnytskaya <154842272+KaterynaKalnytskaya@users.noreply.github.com>
2024-01-10 10:48:33 +0100

    Update lesson_3.py

--- lesson_3/lesson_3.py
+++ lesson_3/lesson_3.py
@@ -1,30 +1,14 @@
+#task number 2
 try:
-    user_select = int(input('Enter number of the day: '))
 
-    match user_select:
-        case 1:
-            print("Sunday")
-        case 2:
-            print("Monday")
-        case 3:
-            print("Tuesday")
-        case 4:
-            print("Wednesday")
-        case 5:
-            print("Thursday")
-        case 6:
-            print("Friday")
-        case 7:
-            print("Saturday")
-        case _:
-            print("Incorrect number")
+    num1 = int(input("Enter number 1: "))
+    num2 = int(input("Enter number 2: "))
+    if num1 == num2:
+        print(num1, num2)
+    else:
+        print(num2, num1)
 except ValueError as error:
     print("Please enter only integer number")
     print(f"ValueError: {error}")
 except Exception as error:
     print(f"Exception occurred: {error}")
-
-
-
-
-
