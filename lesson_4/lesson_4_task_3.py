2d0177b6c20e03a916574aa51a1be6fbd1d5d397
kateryna <katerynakalnytskaya@gmail.com>
2024-01-11 18:17:07 +0100

    added task 1

---
+++ lesson_4/lesson_4_task_3.py
@@ -0,0 +1,5 @@
+# task 3
+some_text = input("Enter text: ")
+word_to_search = input("Enter word to search: ")
+word_to_replace = input("Enter word to replace: ")
+print(some_text.replace(word_to_search, word_to_replace))
