32b159bbc2d2b8be943352cf537890971fa50ba8
KaterynaKalnytskaya <154842272+KaterynaKalnytskaya@users.noreply.github.com>
2024-01-11 21:49:33 +0100

    Update lesson_4_task_2.py

--- lesson_4/lesson_4_task_2.py
+++ lesson_4/lesson_4_task_2.py
@@ -1,1 +1,8 @@
+# task 2
+some_text = input("Enter text: ")
+result_symbol = 0
+for i in some_text:
+    if i == '@':
+        result_symbol += 1
+print(f"The number of symbol '@' in the text:", result_symbol)
 
