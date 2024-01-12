43087e5efd50202ffef3f1e13ab72d7dc8ff376d
kateryna <katerynakalnytskaya@gmail.com>
2024-01-12 11:14:31 +0100

    added a part of dop task

--- lesson_4/lesson_4_task_4.py
+++ lesson_4/lesson_4_task_4.py
@@ -1,5 +1,5 @@
 
-# task 4
+task 4
 
 some_text = "Wellcome home!"
 print(some_text[3])
@@ -12,16 +12,17 @@ print(some_text[::-1])
 print(some_text[::-2])
 print(len(some_text))
 
-#dop
+dop
 
 
-some_text = ("heLLo! mY naMe is Kateryna! I am 37 years old."
-                 " I lIVe in Spain during 2 years. WHat is your name?"
-                 " How old aRE you? Where are yOU from?")
+some_text = ("hello! my name is Kateryna! "
+             "i am 37 years old. i live in Spain during 2 years."
+             " What is your name? How old are you? Where are you from?")
 number_of_digits = 0
 number_of_exclamation = 0
 count_separator = 0
-symbol_list = "!",".","?"
+symbol_list = "! ", ". ", "? "
+
 for i in some_text:
     if i.isdigit():
         number_of_digits += 1
@@ -30,6 +31,22 @@ for i in some_text:
             number_of_exclamation += 1
 print(f"There are:",number_of_digits, "numbers of digit in the text.")
 print(f"There are:",number_of_exclamation,"exclamation points in the text")
+
+
+# И
+x = 10
+SYMBOL = '*'
+for i in range(x):
+    print(" * " * (x - i))
+# б
+x = 10
+SYMBOL = '*'
+for i in range(x):
+    print((0 + i) * " * ")
+
+
+
+
 
 
 
