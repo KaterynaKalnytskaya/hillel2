cc914dbcd001a421a3326080c7eae6697c0238fe
kateryna <katerynakalnytskaya@gmail.com>
2024-01-20 13:39:15 +0100

    just commit

--- lesson_3/lesson_3_task2.py
+++ lesson_3/lesson_3_task2.py
@@ -1,14 +1,17 @@
-#task number 2
-try:
+# #task number 2
+# try:
+#
+#     num1 = int(input("Enter number 1: "))
+#     num2 = int(input("Enter number 2: "))
+#     if num1 == num2:
+#         print(num1, num2)
+#     else:
+#         print(num2, num1)
+# except ValueError as error:
+#     print("Please enter only integer number")
+#     print(f"ValueError: {error}")
+# except Exception as error:
+#     print(f"Exception occurred: {error}")
 
-    num1 = int(input("Enter number 1: "))
-    num2 = int(input("Enter number 2: "))
-    if num1 == num2:
-        print(num1, num2)
-    else:
-        print(num2, num1)
-except ValueError as error:
-    print("Please enter only integer number")
-    print(f"ValueError: {error}")
-except Exception as error:
-    print(f"Exception occurred: {error}")
+
+
