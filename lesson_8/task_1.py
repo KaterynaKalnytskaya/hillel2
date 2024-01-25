bff4024afeebbe72cc26a7fc0c9dd0a3913bf949
KaterynaKalnytskaya <154842272+KaterynaKalnytskaya@users.noreply.github.com>
2024-01-25 14:25:20 +0100

    Update task_1.py

---
+++ lesson_8/task_1.py
@@ -0,0 +1,11 @@
+def get_number_pow(number, number_pow):
+    if number_pow <= 1:
+        return number
+    return number * get_number_pow(number, number_pow - 1)
+
+print(get_number_pow(2,3))
+
+#####################################
+## number_pow 3 -> * number 2 => 8
+## number_pow 2 -> * number 2 => 4
+## number_pow 1 => 2
