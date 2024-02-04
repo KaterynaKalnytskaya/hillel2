8579e0dbdcdafa4cb445a40a918836e2da1c0e97
kateryna <katerynakalnytskaya@gmail.com>
2024-02-04 22:56:01 +0100

    added changes

--- lesson_11/task_1.py
+++ lesson_11/task_1.py
@@ -8,24 +8,19 @@ with open("text1.txt", "w") as test_file:
 
 with open("text1.txt", "r") as test_file:
     text = test_file.read()
-words_in_text = re.findall(r'^[A-Z][a-z]{7,}$', text)
+words_in_text = re.findall(r'\b\w{7,}\b', text)
 
-with open("text2.txt", "a") as new_test_file:
+with open("text2.txt", "w") as new_test_file:
     new_test_file.write(" ".join(words_in_text))
-print("Filtered words have been written to text2.txt")
+    count_of_words = len(words_in_text)
+print("Filtered words have been written to text2.txt", count_of_words)
 
 with open("text1.txt", "r") as test_file:
     text = test_file.read()
-    number_of_words = re.findall(r'\b\w+\b', text)
-    word_count = len(number_of_words)
-print("The number of words in the text: ", word_count)
+forbidden_words = re.findall(r'\b[Dd]ie\b', text)
+text = re.sub(r'\b[Dd]ie\b', '***', text)
+forbidden_word_count = len(forbidden_words)
 
-with open("text1.txt", "r") as test_file:
-    text = test_file.read()
-    forbidden_word = re.findall(r'\b[Dd]ie\b', text)
-    for word in forbidden_word:
-        text = re.sub(r'\b[Dd]ie\b', '***', text)
-        forbidden_word_count = len(forbidden_word)
-with open("text1.txt", "a") as test_file:
+with open("text1.txt", "w") as test_file:
     test_file.write(text)
-print("Forbidden words corrected! The number of words:", forbidden_word_count)
+print("Forbidden words corrected! The number of unique forbidden words:", forbidden_word_count)
