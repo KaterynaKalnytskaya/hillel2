b7c5d70ecf8a566fb173539ea6c904d02748ad1d
kateryna <katerynakalnytskaya@gmail.com>
2024-02-05 10:09:11 +0100

    added changes lesson 5

--- lesson_5/lesson_5_task_1.py
+++ lesson_5/lesson_5_task_1.py
@@ -1,71 +1,34 @@
 numbers = [-1, -4, 8, 3, 2, 4, 7, 3, 7, -9]
-summ = 0
-summ1 = 0
-summ2 = 0
-mult = 1
-elements = []
+
+summ_neg = sum(num for num in numbers if num < 0)
+print("Sum of negative numbers:", summ_neg)
+
+
+summ_even = sum(num for num in numbers if num % 2 == 0)
+print("Sum of even numbers:", summ_even)
+
+
+summ_odd = sum(num for num in numbers if num % 2 != 0)
+print("Sum of odd numbers:", summ_odd)
+
+
+mult_by_three = 1
+for num in numbers:
+    if num % 3 == 0:
+        mult_by_three *= num
+print("Product of numbers divisible by 3:", mult_by_three)
+
+
 min_num = min(numbers)
 max_num = max(numbers)
-for negative_num in numbers:
-    if negative_num < 0:
-        summ += negative_num
-print(summ)
-for i in numbers:
-    if i % 2 == 0:
-        summ1 += i
-print(summ1)
-for j in numbers:
-    if j % 2 != 0:
-        summ2 += j
-print(summ2)
-for q in numbers:
-    if q % 3 == 0:
-        mult *= q
-print(mult)
 result = 1
-for r in numbers:
-    if min_num != r and max_num != r:
-        result *= r
-        print(result)
-first_positive_index = None
-last_positive_index = None
-for i in range(len(numbers)):
-    if numbers[i] > 0:
-        first_positive_index = i
-        break
-for i in range(len(numbers) - 1, -1, -1):
-    if numbers[i] > 0:
-        last_positive_index = i
-        break
-sum_between = sum(numbers[first_positive_index+1:last_positive_index])
+for num in numbers:
+    if num != min_num and num != max_num:
+        result *= num
+print("Product of all numbers except min and max:", result)
+
+
+first_positive_index = next(i for i, num in enumerate(numbers) if num > 0)
+last_positive_index = next(i for i in range(len(numbers) - 1, -1, -1) if numbers[i] > 0)
+sum_between = sum(numbers[first_positive_index + 1:last_positive_index])
 print("Sum between first and last positive elements:", sum_between)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
