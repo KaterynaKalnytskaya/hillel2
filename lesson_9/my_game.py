e61bb90bd27c0272f8dc44a6cf169c42f1d06048
kateryna <katerynakalnytskaya@gmail.com>
2024-02-07 09:38:03 +0100

    added changes to lesson 9

--- lesson_9/my_game.py
+++ lesson_9/my_game.py
@@ -2,26 +2,33 @@ def my_game():
     start = 1
     end = 10
     print(f"Guess a number between {start} and {end}")
+
     while True:
         middle_number = (start + end) // 2
-        print(f"Your number: {middle_number}?")
-        user_input = input('"+"-> yes, "-" -> no')
+        print(f"Is your number {middle_number}?")
+        user_input = input('"+" -> yes, "-" -> no: ')
+
         if user_input == '+':
-            print(f"We guessed your number:{middle_number}")
+            print(f"We guessed your number: {middle_number}")
             return
         elif user_input == '-':
-            print(f"Your number bigger then {middle_number}")
-            user_input = input('"+"-> yes, "-"-> no')
+            print(f"Your number is bigger than {middle_number}")
+            user_input = input('"+" -> yes, "-" -> no: ')
+
             if user_input == "+":
                 start = middle_number + 1
+                if end >= 11:
+                    print("Number is out of range")
+                    return
             elif user_input == "-":
                 end = middle_number - 1
-            elif start < 1:
-                print("Number is out of range")
-            elif end > 10:
-                print("Number is out of range")
+                if start <= 0:
+                    print("Number is out of range")
+                    return
             else:
                 print("Invalid input. Try again.")
+        else:
+            print("Invalid input. Try again.")
 
 
 my_game()
